2324e82bf4824972b2953ead2da70808404f970f
Jared Monk <admin@jaredmonkdigital.com>
2019-07-15 19:12:51 -0500

    Added route to add contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,19 +24,49 @@ router.get('/', auth, async (req, res) => {
 // @route     POST api/contacts
 // @desc      Add new contact
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add Contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 // @route     PUT api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.put('/', (req, res) => {
+router.put('/:id', (req, res) => {
   res.send('Update contact');
 });
 // @route     DELETE api/contacts/:id
 // @desc      Delete contact
 // @access    Private
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
   res.send('Delete contact');
 });
 
