56f51540b5ddac066418de846a65702cc12c57d2
Jared Monk <admin@jaredmonkdigital.com>
2019-07-16 11:34:11 -0500

    scaffolded Context for contacts component

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,71 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Velda Lueilwitz',
+        email: 'Lue_Collins28@yahoo.com',
+        phone: '948-869-5265',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Austin Willms',
+        email: 'Craig2@hotmail.com',
+        phone: '790-654-9381 x4665',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Vance Price',
+        email: 'Alejandra_Graham@hotmail.com',
+        phone: '794.538.6335',
+        type: 'professional'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Actions
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
